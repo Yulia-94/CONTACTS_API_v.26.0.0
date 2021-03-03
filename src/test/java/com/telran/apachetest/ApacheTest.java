f67db13460861c18ff393adab295ae07314aaa5a
Yulia-94 <yrosenblum1994@gmail.com>
2021-03-03 10:54:42 +0200

    lestslogin

--- src/test/java/com/telran/apachetest/ApacheTest.java
+++ src/test/java/com/telran/apachetest/ApacheTest.java
@@ -1,13 +1,22 @@
 package com.telran.apachetest;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.telran.dto.AuthRequestDto;
+import com.telran.dto.AuthResponseDto;
+import com.telran.dto.ErrorDto;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
 import org.apache.http.entity.ContentType;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 
 public class ApacheTest {
 
@@ -32,6 +41,48 @@ public class ApacheTest {
         JsonElement element = JsonParser.parseString(responseJson);
         JsonElement token = element.getAsJsonObject().get("token");
         System.out.println(token.getAsString());
+
+    }
+    @Test
+    public void testLogin() throws IOException {
+        AuthRequestDto requestDto= AuthRequestDto.builder()
+                .email("john9090@mail.com").password("Aa12345~").build();
+        Gson gson = new Gson();
+        Response response = Request.Post("https://contacts-telran.herokuapp.com/api/login")
+                .bodyString(gson.toJson(requestDto), ContentType.APPLICATION_JSON)
+                .execute();
+        String responseJson = response.returnContent().asString();
+        AuthResponseDto responseDto = gson.fromJson(responseJson, AuthResponseDto.class);
+        System.out.println(responseDto.getToken());
+
+
+    }
+
+    @Test
+    public void testLogin1() throws IOException {
+        AuthRequestDto requestDto= AuthRequestDto.builder()
+                .email("john9090@mail.com").password("a12345~").build();
+        Gson gson = new Gson();
+        Response response = Request.Post("https://contacts-telran.herokuapp.com/api/login")
+                .bodyString(gson.toJson(requestDto), ContentType.APPLICATION_JSON)
+                .execute();
+        HttpResponse httpResponse= response.returnResponse();
+        System.out.println(httpResponse);
+        System.out.println(httpResponse.getStatusLine().getStatusCode());
+
+        InputStream is = httpResponse.getEntity().getContent();
+        BufferedReader br= new BufferedReader(new InputStreamReader(is));
+        String line;
+        StringBuilder sb = new StringBuilder();
+        while((line = br.readLine())!= null){
+            sb.append(line);
+        }
+        System.out.println(sb.toString());
+        ErrorDto error = gson.fromJson(sb.toString(), ErrorDto.class);
+        System.out.println(error.getCode());
+        System.out.println(error.getMessage());
+
+
 
     }
 
--- src/test/java/com/telran/okhttptest/OkHttpTest.java
+++ src/test/java/com/telran/okhttptest/OkHttpTest.java
@@ -1,10 +1,47 @@
 package com.telran.okhttptest;
 
-import okhttp3.MediaType;
+import com.google.gson.Gson;
+import com.telran.dto.AuthRequestDto;
+import com.telran.dto.AuthResponseDto;
+import com.telran.dto.ErrorDto;
+import okhttp3.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class OkHttpTest {
     public static final MediaType JSON
             = MediaType.get("application/json; charset=utf-8");
+
+    @Test
+    public void testLogin() throws IOException {
+        Gson gson = new Gson();
+        OkHttpClient client = new OkHttpClient();
+        AuthRequestDto requestDto= AuthRequestDto.builder()
+                .email("john9090@mail.com").password("a12345~").build();
+
+
+        RequestBody requestBody = RequestBody.create(gson.toJson(requestDto), JSON);
+        Request request = new Request.Builder()
+                .url("https://contacts-telran.herokuapp.com/api/login")
+                .post(requestBody).build();
+
+
+        Response response = client.newCall(request).execute();
+        String responseJson = response.body().string();
+        if(response.isSuccessful()){
+
+            AuthResponseDto responseDto = gson.fromJson(responseJson, AuthResponseDto.class);
+            System.out.println(responseDto.getToken());
+
+        } else {
+            ErrorDto errorDto =  gson.fromJson(responseJson, ErrorDto.class);
+            System.out.println(errorDto.getCode());
+            System.out.println(errorDto.getMessage());
+
+        }
+
+    }
 
 
 
